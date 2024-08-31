f3cb1f67190a1073574f410a8ca69d6eca9e3b54
goto at home <goto-sh@cheng.es.osaka-u.ac.jp>
2024-08-31 15:42:58 +0900

    依存性の確認をinstall.sh についか

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+if [ ! -d ~/.config ]; then
+    mkdir ~/.config
+fi
+
 slinkfiles=(.vimrc .tmux.conf .zsh)
 for file in "${slinkfiles[@]}"; do
     ln -svf $PWD/$file ~/
@@ -14,3 +18,26 @@ contents=(sheldon nvim rofi alacritty)
 for config in "${contents[@]}"; do
     ln -svf $PWD/.config/$config ~/.config
 done
+
+# 依存性チェック
+if [ ! -x "$(command -v nvim)" ]; then
+    echo "nvim is not installed"
+fi
+if [ ! -x "$(command -v zsh)" ]; then
+    echo "zsh is not installed"
+fi
+if [ ! -x "$(command -v cargo)" ]; then
+    echo "cargo is not installed"
+fi
+if [ ! -x "$(command -v uv)"]; then
+    echo "uv is not installed"
+fi
+if [ ! -x "$(command -v tmux)" ]; then
+    echo "tmux is not installed"
+fi
+if [ ! -x "$(command -v rofi)" ]; then
+    echo "rofi is not installed"
+fi
+if [ ! -x "$(command -v alacritty)" ]; then
+    echo "alacritty is not installed"
+fi
