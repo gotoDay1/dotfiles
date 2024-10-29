640bc392b09d39c783b66d405856be176b7584f1
shota goto <sg@myMacMini.local>
2024-10-29 23:32:17 +0900

    ￥ → \

--- .config/nvim/lua/plugins/fern.lua
+++ .config/nvim/lua/plugins/fern.lua
@@ -1,6 +1,7 @@
 return{
     {
         "lambdalisue/fern.vim",
+        event = "BufEnter",
         config = function()
             vim.keymap.set("n", "<C-a>", ":Fern %:hr -reveal=%<CR>", {silent = true})
         end,
