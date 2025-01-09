721a7a92e9cac925a991ff8d6439e430d390950f
Shota Goto <49383594+gotoshota@users.noreply.github.com>
2025-01-09 13:10:37 +0900

    copilotを使用するように。copilot.vimと競合しないのかをチェックする必要あり。

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -6,7 +6,46 @@ return{
   version = false, -- set this if you want to always pull the latest change
   opts = {
     -- add any opts here
-  },
+    provider = "copilot",
+    auto_suggestions_provider = "copilot",
+    behaviour = {
+            auto_suggestions = true,
+            auto_set_highlight_group = true,
+            auto_set_keymaps = true,
+            auto_apply_diff_after_generation = true,
+            support_paste_from_clipboard = true,
+        },
+        windows = {
+            position = "right",
+            width = 30,
+            sidebar_header = {
+                align = "center",
+                rounded = false,
+            },
+            ask = {
+                floating = true,
+                start_insert = true,
+                border = "rounded"
+            }
+        },
+        -- providers-setting
+        claude = {
+            model = "claude-3-5-sonnet-20240620", -- $3/$15, maxtokens=8000
+            -- model = "claude-3-opus-20240229",  -- $15/$75
+            -- model = "claude-3-haiku-20240307", -- $0.25/1.25
+            max_tokens = 8000,
+        },
+        copilot = {
+            model = "gpt-4o-2024-05-13",
+            -- model = "gpt-4o-mini",
+            max_tokens = 4096,
+        },
+        openai = {
+            model = "gpt-4o", -- $2.5/$10
+            -- model = "gpt-4o-mini", -- $0.15/$0.60
+            max_tokens = 4096,
+        },
+    },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
