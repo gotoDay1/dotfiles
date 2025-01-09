59b4a75f9135b71134de4d97e6a523b79d1dc454
Shota Goto <49383594+gotoshota@users.noreply.github.com>
2025-01-09 15:46:16 +0900

    auto_suggestionはcopilot.vimにsita

--- .config/nvim/lua/plugins/fern.lua
+++ .config/nvim/lua/plugins/fern.lua
@@ -3,7 +3,7 @@ return{
         "lambdalisue/fern.vim",
         lazy = false,
         config = function()
-            vim.keymap.set("n", "<C-a>", ":Fern %:hr -reveal=%<CR>", {silent = true})
+            vim.keymap.set("n", "<C-a>", ":Fern %:h -drawer -toggle -reveal=% -width=45 <CR>", {silent = true})
         end,
     },
 }
