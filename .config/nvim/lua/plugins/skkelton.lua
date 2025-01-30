891fd462eb42919daf2aa086dcc5b9f56b27ae8c
Shota Goto <49383594+gotoshota@users.noreply.github.com>
2025-01-30 14:16:07 +0900

    <C-k> でskkeltonをトグルするように

--- .config/nvim/lua/plugins/skkelton.lua
+++ .config/nvim/lua/plugins/skkelton.lua
@@ -9,9 +9,10 @@ return {
                     { '~/.skk/SKK-JISYO.L', 'euc-jp'},  
                 },
             })
+
             -- Keymapの設定
-            vim.api.nvim_set_keymap('i', '<C-k>', '<Plug>(skkeleton-enable)', { noremap = false })
-            vim.api.nvim_set_keymap('c', '<C-k>', '<Plug>(skkeleton-enable)', { noremap = false })
+            vim.api.nvim_set_keymap('i', '<C-k>', '<Plug>(skkeleton-toggle)', { noremap = false })
+            vim.api.nvim_set_keymap('c', '<C-k>', '<Plug>(skkeleton-toggle)', { noremap = false })
             -- skkeletonのモードを取得して、ステータスラインに表示
             vim.o.statusline = vim.o.statusline .. '%{skkeleton#mode()}'
         end,
