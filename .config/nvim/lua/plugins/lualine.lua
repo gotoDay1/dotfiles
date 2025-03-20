98cbac4f0b09692ac6366093cf7f275efa9776ac
shota goto <sg@myMacMini.local>
2025-03-21 02:32:26 +0900

    feat: add lualine configuration for enhanced status line features

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,25 @@
+return {
+    {
+        'nvim-lualine/lualine.nvim',
+        event = 'VeryLazy',
+        dependencies = {
+            'nvim-tree/nvim-web-devicons',
+        },
+        opts = {
+        },
+        section = {
+            lualine_a = {'mode'},
+            lualine_b = {'branch', 'diff', 'diagnostics'},
+            lualine_c = {
+                {
+                    'filename',
+                    path = 2
+                }
+            },
+            lualine_x = {'encoding', 'fileformat', 'filetype'},
+            lualine_y = {'progress'},
+            lualine_z = {'location'}
+        },
+    }
+}
+
