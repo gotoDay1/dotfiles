d394d7fc009a91c0e781b1cd899fd49d921d8745
gotoday1 <gotoshota.gs@gmail.com>
2025-06-30 22:07:18 +0900

    update

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,22 +1,24 @@
 Set-Alias -Name vim -Value nvim
 Set-Alias -Name vi -Value nvim
 
-oh-my-posh init pwsh --config $env:POSH_THEMES_PATH/pure.omp.json | Invoke-Expression
+# oh-my-posh
+Set-Variable theme pure
+oh-my-posh init pwsh --config $env:POSH_THEMES_PATH/$theme.omp.json | Invoke-Expression
 
 # emacs 風ショートカットに
 Import-Module PSReadline
 Set-PSReadLineOption -EditMode Emacs
 
-Set-PSReadLineOption -PredictionSource History
 
 # Tab補完をzsh風に 
+Set-PSReadLineOption -PredictionSource History
+# その他補完機能
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadlineKeyHandler -Key Ctrl+p -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key Ctrl+n -Function HistorySearchForward
-
 
 
 # Fzf
